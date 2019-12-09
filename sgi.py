8a92c91ac9fdefd6bd5d64f1912229af0d3995c7
Francisco López <franlopez.freelance@gmail.com>
2019-12-08 22:29:34 -0300

    Ejemplo de Microservicio en Python

---
+++ sgi.py
@@ -0,0 +1,16 @@
+import json
+from wsgiref.simple_server import make_server
+
+def application(environ, start_response):
+    headers = [('Content-type', 'application/json')]
+
+    start_response('200 OK', 'headers')
+
+    response = {
+        'message' : 'Hola Mundo desde el Servidor'
+    }
+
+    return [ bytes(json.dumps(response), 'utf-8')]
+
+server = make_server('localhost', 8000, application)
+server.handle_request()
